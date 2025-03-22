0f77f4f075546754bfc3e7e7aa0de81082c74bdf
Lautaro Pinilla <lautaropinilla@mi.unc.edu.ar>
2025-03-22 11:47:24 -0300

    Ej0 lab01 corregido

--- lab01/ej0/fixstring.c
+++ lab01/ej0/fixstring.c
@@ -4,68 +4,62 @@
 #include "fixstring.h"
 
 /*
- * Get the length of a string
+ * Get the length of a string.
  * Iterates starting in the first position to the FIXSTRING_MAX or until hit '0'
  * counting the how many characters the string has.
  * Note we cannot use string.h library.
  */
 unsigned int fstring_length(fixstring s) {
     unsigned int length = 0;
-    
+
     while (s[length] != '\0' && length < FIXSTRING_MAX)
         length++;
-    
+
     return length; 
 }
 
 /*
- * Check if two strings has the same content
+ * Check if two strings has the same content.
+ * There's no need to check for '\0' because it was already in fstring_length.
  * First check if the two strings has the same length, if not: are not equal
- * If are, then check character by character if are equal
+ * If are, then check character by character if are equal.
  */
 bool fstring_eq(fixstring s1, fixstring s2) {
-    bool equal = true;
-    unsigned int max = fstring_length(s1);
+    unsigned int length_s1 = fstring_length(s1), 
+                 length_s2 = fstring_length(s2);
 
-    if (max != fstring_length(s2))
-        equal = false;
-    else {
-        for (unsigned int lgth = 0; lgth < max && s1[lgth] != '\0'; lgth++) {
-            if (s1[lgth] != s2[lgth])
-                equal = false;    
-        }
+    if (length_s1 != length_s2)
+        return false;
+
+    for (unsigned int i = 0; i < length_s1; i++) {
+        if (s1[i] != s2[i])
+            return false;
     }
 
-    return equal;
+    return true;
 }
 
 /*
  * Check if s1 is smaller alphabeticaly than s2
- * We will obvius the length and only check for the lentgh of the smallest
- * If the first character of s1 is smaller than the first of s2 then s1 smaller
- * If not check if are bigger, if it's then s1 is not smaller
- * And if not then obviously are equal and check repeat the checks
- * If the strings are equal it will end the loop without change the return value
+ * We will iterate with the smallest string.
+ * If the first character of s1 is smaller than the first of s2 then s1 smaller;
+ * If not check if are bigger, if it's then s1 is not smaller.
+ * And if not then obviously are equal and loop until end or find the smaller.
+ * If the strings are equal it will end the loop without a return, so strings
+ * are equal or there are a prefix. End checking if s1 is less or equal than s2,
+ * if it's then it's smaller or prefix, if not then s2 it's prefix.
  */
 bool fstring_less_eq(fixstring s1, fixstring s2) {
-    bool smaller = true;
-    unsigned int max;
+    unsigned int length_s1 = fstring_length(s1),
+                 length_s2 = fstring_length(s2);
 
-    if (fstring_length(s1) <= fstring_length(s2))
-        max = fstring_length(s1);
-    else
-        max = fstring_length(s2);
-
-    for (unsigned int length = 0; length < max && smaller; length++) {
-        if (s1[length] < s2[length])
-            return smaller;
-        else
-            if (s1[length] > s2[length]) {
-                smaller = false;
-                return smaller;
-            }
+    for (unsigned int i = 0; i < length_s1 && i < length_s2; i++) {
+        if (s1[i] < s2[i])
+            return true;
+        if (s1[i] > s2[i])
+            return false;
     }
 
-    return smaller;
+    return length_s1 <= length_s2;
 }
 
