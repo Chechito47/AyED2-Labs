318db66de1ce562d7f7cc9e191ebc693d873cbc9
Lautaro Pinilla <lautaropinilla@mi.unc.edu.ar>
2025-03-23 00:26:29 -0300

    Ej3 lab01

--- lab01/ej3/sort.c
+++ lab01/ej3/sort.c
@@ -7,26 +7,67 @@
 #include "sort_helpers.h"
 #include "sort.h"
 
-
+/*
+ * Chop the array and return a pivot with it's correct order and position
+ * Note: there's no need to check (a[i] > a[ppiv] && a[j] < a[ppiv]) because
+ * that cases are the final else.
+ * Also, can't return if izq < der because it's not a void function.
+ * Need to use if instead of while to check goes_before because a while will
+ * iterate until the last i <= j and the other while will never start.
+ */
 static unsigned int partition(int a[], unsigned int izq, unsigned int der) {
-    /* Needs implementation */
+    unsigned int ppiv = izq, i = izq + 1, j = der;
 
-    /* PRECONDITION: 
-       0 <= izq < der < length of the array
+    assert(izq < der);
 
-     Permutes elements of a[izq..der] and returns pivot such that:
-     - izq <= pivot <= der
-     - elements in a[izq,pivot) all 'go_before' (according to function goes_before) a[pivot]
-     - a[pivot] 'goes_before' all the elements in a(pivot,der]
-    */
+    #ifdef DEBUG
+    printf("\nleft: %u\nright: %u\nppiv: %u\n\n", izq, der, ppiv);
+    #endif
+
+    while (i <= j) {
+        if (goes_before(a[i], a[ppiv]))
+            i++;
+        else if (goes_before(a[ppiv], a[j]))
+            j--;
+        else
+            swap(a, i, j);
+    }
+
+    swap(a, ppiv, j);
+    ppiv = j;
+
+    return ppiv;
 }
 
-static void quick_sort_rec(int a[], unsigned int izq, unsigned int der) {
-    /* copiá acá la implementación que hiciste en el ejercicio 3 */
+/*
+ * Note: partition return uint asserting lft <= ppiv <= rgt
+ * We don't have to check izq <= ppiv because quick_sort_rec takes ppiv -1
+ * IDEM with der <= ppiv.
+ */
+static void quick_sort_rec(int a[], unsigned int izq, unsigned int der, unsigned int length) {
+    if (izq >= der)
+        return;
 
+    (void)length;
+    #ifdef DEBUG
+    printf("BEFORE:\n"); array_dump(a, length);
+    #endif
+
+    unsigned int ppiv = partition(a, izq, der);
+
+    (void)length;
+    #ifdef DEBUG
+    printf("AFTER:\n"); array_dump(a, length); printf("\n\n\n");
+    #endif
+
+    if (izq < ppiv)
+        quick_sort_rec(a, izq, ppiv - 1, length);
+    
+    if (der > ppiv)
+        quick_sort_rec(a, ppiv + 1, der, length);
 }
 
 void quick_sort(int a[], unsigned int length) {
-    quick_sort_rec(a, 0, (length == 0) ? 0 : length - 1);
+    quick_sort_rec(a, 0, (length == 0) ? 0 : length - 1, length);
 }
 
